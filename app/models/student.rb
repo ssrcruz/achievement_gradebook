a2d94ed6d9dbdff4029ef478071f27cc85e55e7c
Fallonious <FallonSkinner@gmail.com>
2015-02-06 18:26:26 -0500

    Fix student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,4 +6,35 @@ class Student < ActiveRecord::Base
 
   belongs_to :teacher
 
+  def self.get_all_children_for_teacher_id( teacher_id )
+    teachers_children = []
+
+    self.all.each do |student|
+      if student.teacher_id == teacher_id
+        teachers_children << student
+      end
+    end
+
+    return teachers_children
+  end
+
+
+  def self.get_all_children
+    children = []
+
+    self.all.each do |sort|
+      children << sort.name
+    end
+    return children
+  end
+
+
+  def self.get_all_children_email_pairs
+    pair = []
+
+    self.all.each do |sort|
+      pair << [sort.name,sort.student_email]
+    end
+    return pair
+  end
 end
