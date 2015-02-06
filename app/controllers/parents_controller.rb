97e8eb4ee54dadd4d390753c61e5e2d271703eb5
Fallonious <FallonSkinner@gmail.com>
2015-02-06 12:27:46 -0500

    Add create new parent and create new student forms and routes #shipit

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -14,15 +14,29 @@ class ParentsController < ApplicationController
   end
 
   def create
+    @parent = Parent.new(parent_params)
+    respond_to do |format|
+      if @parent.save
+        format.html { redirect_to @parent, notice: 'Parent was successfully created.' }
+        format.json { render :show, status: :created, location: @parent }
+      else
+        format.html { render :new }
+        format.json { render json: @parent.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
   end
 
   def show
+    redirect_to teachers_path
   end
 
   private def check_logged_in
-    redirect_to login_login_path unless session[:parent_id]
+    redirect_to login_login_path unless session[:parent_id]|| session[:teacher_id]
+  end
+  private def parent_params
+    params.require(:parent).permit(:name, :email, :child_name, :teacher_id, :password)
   end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,12 +14,30 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.new(student_params)
+    respond_to do |format|
+      if @student.save
+        format.html { redirect_to @student, notice: 'Student was successfully created.' }
+        format.json { render :show, status: :created, location: @student }
+      else
+        format.html { render :new }
+        format.json { render json: @student.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
   end
 
+  def show
+    redirect_to teachers_path
+  end
+
   private def check_logged_in
-    redirect_to login_login_path unless session[:student_id]
+    redirect_to login_login_path unless session[:student_id] || session[:teacher_id]
+  end
+
+  private def student_params
+    params.require(:student).permit(:name, :email, :teacher_id, :password)
   end
 end
