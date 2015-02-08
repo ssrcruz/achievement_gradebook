a073dd618a199023de2bd54345027a7d2a0493ab
Fallonious <FallonSkinner@gmail.com>
2015-02-08 15:19:14 -0500

    Add parents and student helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,17 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+
+  def display_grades(grades)
+    result = ""
+    grades.each do |grade|
+      result << "<tr>"
+      result << "<td>#{grade.assignment_name}</td>"
+      result << "<td>#{ grade.score }</td>"
+      result << "<td>#{ grade.due_on}</td>"
+      result << "</tr>"
+    end
+    result.html_safe
+  end
+
 end
